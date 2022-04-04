b8c3ce2c32d50bcab34a631824b439b9f36b2588
caiofov <cfoviana@gmail.com>
2022-04-03 22:04:41 -0300

    instructions

--- config.js
+++ config.js
@@ -7,17 +7,7 @@ CNV_WIDTH = 800
 //pace of the figure: how many pixels it will walk to the next generated point
 const PACE = 5 
 
-//L-SYSTEM
-//it's the system of rules the koch curve will folllow to generate its path. 
-//Consider separating the rules by hierachy for better results.
-//example of hierarchy:
-//1. A -> AB
-//2. B -> A
-//you should write:
-//L_SYSTEM_LEFT = ["A", "B"]
-// L_SYSTEM_RIGHT = ["AB", A]
-
-//example used: f -> f+f-f-f+f
+//L-SYSTEM - rules of transformation (if you have no idea how to use it, please consider reading the instructions in readme.md file)
 const L_SYSTEM_LEFT = ["f"]
 const L_SYSTEM_RIGHT = ["f+f-f-f+f"]
 
@@ -29,5 +19,3 @@ const INITIAL_Y = CNV_HEIGHT/2
  
 
 const EXPANSIONS = 3 //number of iterations in order to generate a greater figure
-
-// - - - - - - - - - - - - - - - - - - - - - - - -
